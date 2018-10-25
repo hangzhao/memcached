85edf8bb8078e7419995675d4b8a48ca23487c47
zhao <1302036775@qq.com>
2018-10-25 15:21:46 +0800

    dd

---
+++ memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/zhao/github/memcached-1.5.11/assoc.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/assoc.c.o"
+  "/Users/zhao/github/memcached-1.5.11/bipbuffer.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/bipbuffer.c.o"
+  "/Users/zhao/github/memcached-1.5.11/cache.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/cache.c.o"
+  "/Users/zhao/github/memcached-1.5.11/crawler.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/crawler.c.o"
+  "/Users/zhao/github/memcached-1.5.11/crc32c.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/crc32c.c.o"
+  "/Users/zhao/github/memcached-1.5.11/daemon.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/daemon.c.o"
+  "/Users/zhao/github/memcached-1.5.11/extstore.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/extstore.c.o"
+  "/Users/zhao/github/memcached-1.5.11/hash.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/hash.c.o"
+  "/Users/zhao/github/memcached-1.5.11/items.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/items.c.o"
+  "/Users/zhao/github/memcached-1.5.11/itoa_ljust.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/itoa_ljust.c.o"
+  "/Users/zhao/github/memcached-1.5.11/jenkins_hash.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/jenkins_hash.c.o"
+  "/Users/zhao/github/memcached-1.5.11/linux_priv.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/linux_priv.c.o"
+  "/Users/zhao/github/memcached-1.5.11/logger.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/logger.c.o"
+  "/Users/zhao/github/memcached-1.5.11/memcached.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/memcached.c.o"
+  "/Users/zhao/github/memcached-1.5.11/murmur3_hash.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/murmur3_hash.c.o"
+  "/Users/zhao/github/memcached-1.5.11/openbsd_priv.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/openbsd_priv.c.o"
+  "/Users/zhao/github/memcached-1.5.11/sasl_defs.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/sasl_defs.c.o"
+  "/Users/zhao/github/memcached-1.5.11/sizes.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/sizes.c.o"
+  "/Users/zhao/github/memcached-1.5.11/slab_automove.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/slab_automove.c.o"
+  "/Users/zhao/github/memcached-1.5.11/slab_automove_extstore.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/slab_automove_extstore.c.o"
+  "/Users/zhao/github/memcached-1.5.11/slabs.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/slabs.c.o"
+  "/Users/zhao/github/memcached-1.5.11/solaris_priv.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/solaris_priv.c.o"
+  "/Users/zhao/github/memcached-1.5.11/stats.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/stats.c.o"
+  "/Users/zhao/github/memcached-1.5.11/storage.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/storage.c.o"
+  "/Users/zhao/github/memcached-1.5.11/testapp.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/testapp.c.o"
+  "/Users/zhao/github/memcached-1.5.11/thread.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/thread.c.o"
+  "/Users/zhao/github/memcached-1.5.11/timedrun.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/timedrun.c.o"
+  "/Users/zhao/github/memcached-1.5.11/util.c" "/Users/zhao/github/memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ memcached-1.5.11/cmake-build-debug/CMakeFiles/memcached_1_5_11.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memcached_1_5_11.dir/assoc.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/bipbuffer.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/cache.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/crawler.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/crc32c.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/daemon.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/extstore.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/hash.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/items.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/itoa_ljust.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/jenkins_hash.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/linux_priv.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/logger.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/memcached.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/murmur3_hash.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/openbsd_priv.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/sasl_defs.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/sizes.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/slab_automove.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/slab_automove_extstore.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/slabs.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/solaris_priv.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/stats.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/storage.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/testapp.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/thread.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/timedrun.c.o"
+  "CMakeFiles/memcached_1_5_11.dir/util.c.o"
+  "memcached_1_5_11.pdb"
+  "memcached_1_5_11"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/memcached_1_5_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
